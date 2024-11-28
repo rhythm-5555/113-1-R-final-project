9ac4e53701fadcbf3738af4be431d9ab11c4a5a2
rhythm-5555 <s411373002@gm.ntpu.edu.tw>
2024-11-28 15:04:06 +0000

    data

--- R Final Project.R
+++ R Final Project.R
@@ -1,2 +1,14 @@
 library(tidyverse)
-#say hi
+glimpse(data)
+
+# Rename columns for better readability
+data <- data %>%
+  rename(
+    country = `國別`,
+    year = `西元年`,
+    category = `類別`,
+    population_65_plus_pct = `65歲以上人口占總人口比率(%)`,
+    description = `資料說明`,
+    source = `資料來源`
+  )
+
